86d6d980502d710d0539848c6ec0c03d6c66baad
Blutimauge <ilpillo90@gmail.com>
2014-12-10 14:49:13 +0100

    push branch prova

--- src/paoledo/app/theunknownepisode/Activities/MainActivity.java
+++ src/paoledo/app/theunknownepisode/Activities/MainActivity.java
@@ -38,7 +38,8 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+    	super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_drawer);
 
