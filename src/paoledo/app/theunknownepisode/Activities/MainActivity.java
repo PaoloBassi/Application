0c2232f8327ccafcf156efaca45fac68bd16bafd
Blutimauge <ilpillo90@gmail.com>
2014-12-15 17:05:45 +0100

    Now the icons are showing, added an option to the manifest to bring the action bar to the bottom.

--- src/paoledo/app/theunknownepisode/Activities/MainActivity.java
+++ src/paoledo/app/theunknownepisode/Activities/MainActivity.java
@@ -177,7 +177,7 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
 
