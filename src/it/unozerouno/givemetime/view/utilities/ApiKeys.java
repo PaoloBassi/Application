17e2587fec5b1a1d639a4d5f2463a044dd9976d5
Blutimauge <ilpillo90@gmail.com>
2015-01-17 01:32:13 +0100

    commit ApiKeys, gitignore should avoid it B)

---
+++ src/it/unozerouno/givemetime/view/utilities/ApiKeys.java
@@ -0,0 +1,14 @@
+package it.unozerouno.givemetime.view.utilities;
+
+/**
+ * This class contains the api keys for testing. Do not commit
+ * @author Edoardo Giacomello <edoardo.giacomello1990@gmail.com>
+ *
+ */
+public class ApiKeys {
+	public static String API_KEY = "AIzaSyAEhZAOJnRUbgBc9onGCTgUenEOQdNI8Ws";
+
+	public static String getKey() {
+		return API_KEY;
+	}
+}
