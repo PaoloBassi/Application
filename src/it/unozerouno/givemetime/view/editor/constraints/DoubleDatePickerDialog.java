d0aae39fb73d866daec7bd8b86d7530ff1925ed8
Edoardo Giacomello <edoardo.giacomello1990@gmail.com>
2015-01-20 18:28:53 +0100

    Constraint UI update

---
+++ src/it/unozerouno/givemetime/view/editor/constraints/DoubleDatePickerDialog.java
@@ -0,0 +1,13 @@
+package it.unozerouno.givemetime.view.editor.constraints;
+
+import android.app.DialogFragment;
+
+public class DoubleDatePickerDialog extends DialogFragment{
+	
+	
+	
+
+	 class OnConstraintSelectedListener{
+		
+	}
+}
